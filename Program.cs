3c14aa215aa846c97f63cdc994078111103e7e65
Ivan <sheva134@mail.ru>
2023-03-29 00:39:44 +0300

    поиск строк =<3 символов

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿class Program 
+{
+    static void Main(string[] args) 
+    {
+        
+        Console.Write("Введите элементы массива через пробел: ");
+        string[] inputArray = Console.ReadLine().Split(' ');
+        string[] outputArray = new string[inputArray.Length];
+        int count = 0;
+        for (int i = 0; i < inputArray.Length; i++) 
+        {
+            if (inputArray[i].Length <= 3) 
+            {
+                outputArray[count] = inputArray[i];
+                count++;
+            }
+        }
